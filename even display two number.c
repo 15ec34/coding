3d9efe8d0e4aaba954ad1a1a36871f6ec7dbf5b1
15ec34 <36726593+15ec34@users.noreply.github.com>
2018-03-21 16:18:34 +0530

    Create even display two number.c

---
+++ even display two number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+int num,a =1,b=4;
+printf("Print even Numbers in a given range m to n:\n");
+for (num = a; num <= b; num++)
+{
+if (num % 2 == 1)
+ printf ("%d ", num);
+ }
+getch();
+}
