9e11e74b8640c2d5578df1bf678d584897625921
15ec34 <36726593+15ec34@users.noreply.github.com>
2018-03-21 16:49:22 +0530

    Create armstrong number.c

---
+++ armstrong number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+int num,a =1,b=5;
+printf("Print armstrong Numbers in a given range m to n:\n");
+for (num = a; num <= b; num++)
+{
+if (num % 2 == 1)
+ printf ("%d ", num);
+ }
+getch();
+}
