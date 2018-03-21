d8080dc39d4a93d939593fffa7fbe6e00cb40bb4
15ec34 <36726593+15ec34@users.noreply.github.com>
2018-03-21 16:23:59 +0530

    Create primedisplay two interval.c

---
+++ primedisplay two interval.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+int num,a =3,b=3;
+printf("Print prime Numbers in a given range m to n:\n");
+for (num = a; num <= b; num++)
+{
+if (num % 2 == 1)
+ printf ("%d ", num);
+ }
+getch();
+}
